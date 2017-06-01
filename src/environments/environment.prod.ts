14b7cec6ddad3603d013623fa950095f49cd07ad
Antonio Barbatelli <seant_tre@yahoo.it>
2017-06-01 10:35:36 +0200

    api key update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_KEY:"AIzaSyBSnGWkvGIBpXONyAKv5Gt3OofBv0IaHkw",
+  API_KEY:"PUT_YOUR__YOUTUBE_API_KEY",
   YT_BASE:"https://www.googleapis.com/youtube/v3"
 };
